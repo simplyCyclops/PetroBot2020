204a5e97411e7ac942a2c22bfcbde35e637be552
BuildTools <unconfigured@null.spigotmc.org>
2019-08-08 13:41:34 +0300

    Add sample run

---
+++ src/runs/SampleRun.java
@@ -0,0 +1,18 @@
+package runs;
+
+import robot.utils.RobotRun;
+
+/**
+ * Sample robot run
+ * @author John
+ *
+ */
+public class SampleRun extends RobotRun{
+
+	@Override
+	public void runInstructions() {
+		/* RobotMap.getInstance().sampleLeftMotor.forward();
+		 * Wait.waitForSeconds(3);
+		 * RobotMap.getInstance().sampleLeftMotor.stop(); */
+	}
+}
