bef78d58fdc1399629f1b5cc43c1a0e1a75c213e
maipe <maipe@DESKTOP-MQ7R8PL>
2019-11-08 10:12:50 +0200

    add test run

---
+++ src/user/runs/GyroCheck.java
@@ -0,0 +1,16 @@
+package user.runs;
+
+import robot.runs.RobotRun;
+
+public class GyroCheck extends RobotRun{
+
+	public GyroCheck(String name) {
+		super(name);
+	}
+
+	@Override
+	public void runInstructions() {
+		
+	}
+
+}
