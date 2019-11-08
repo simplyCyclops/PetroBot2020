34d2d7f84202ea6ff11e93da8c8549a6a5f2598a
maipe <maipe@DESKTOP-MQ7R8PL>
2019-11-08 10:13:00 +0200

    define hardware

--- src/user/HardwareCreator.java
+++ src/user/HardwareCreator.java
@@ -1,7 +1,12 @@
 package user;
 
+import robot.hardware.chassis.RobotChassis;
+import robot.hardware.sensors.GyroSensor;
+
 public class HardwareCreator {
 	public static void init() {
-			//init your hardware here
+		new RobotChassis("lWheel", 'B', "rWheel", 'C');
+		
+		new GyroSensor("gyro", 1);
 	}
 }
