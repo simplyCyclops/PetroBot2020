9b56e842e7435f384018c62e06f0476bfc016471
simplyCyclops <maipez04@gmail.com>
2019-11-14 21:19:39 +0200

    Finished lineFollow.followDegrees

--- src/user/utils/LineFollow.java
+++ src/user/utils/LineFollow.java
@@ -4,11 +4,19 @@ import robot.RobotMap;
 
 public class LineFollow {
 	
-	public static void followDegrees(int p0, int distance, int light, double kp, boolean brake) {
+	public static void followDegrees(int p0, int distance, int light, double kp, boolean followLeft, boolean brake) {
 		
 		//reset the wheels' rotation sensor
 		RobotMap.getMotor("lWheel").resetEncoder();
 		RobotMap.getMotor("rWheel").resetEncoder();
+		
+		
+		String sensor = "lColor";
+		if(!followLeft) sensor = "rColor"; //determines the sensor to be followed
+		
+		int correctionMod; //the modifier for adding / subtracting speed
+
+		
 		
 		//start drive
 		RobotMap.getChassis().tankDrive(p0, p0);
@@ -16,29 +24,31 @@ public class LineFollow {
 		//drive until distance is reached
 		while(Math.abs(RobotMap.getMotor("lWheel").readEncoder()) < General.Conversion.cmToDegrees(distance) 
 				|| Math.abs(RobotMap.getMotor("lWheel").readEncoder()) < General.Conversion.cmToDegrees(distance)) {
+
+			correctionMod = 1; //sets the correction modifier to add speed
 			
-			double lCorrection = 0;
-			double rCorrection = 0;
-			
-			//correct left wheel if is off color
-			if(RobotMap.getSensor("lColor").read() > light + 0.05
-					|| RobotMap.getSensor("lColor").read() < light - 0.05) {
-				lCorrection = kp;
+			//if sensor is on color - move in opposite direction
+			if(RobotMap.getSensor(sensor).read() < light + 0.05
+					|| RobotMap.getSensor(sensor).read() > light - 0.05) {
+				followLeft = true;
 			}
-			//correct right wheel if is off color
-			if(RobotMap.getSensor("rColor").read() > light + 0.05
-					|| RobotMap.getSensor("rColor").read() < light - 0.05) {
-				rCorrection = kp;
+			//if sensor is not on color move toward color
+			else {
+				followLeft = false;
 			}
 			
-			//stop robot if both sensors don't detect a line
-			if(lCorrection == kp && rCorrection == kp) {
-				General.stopRobot(brake);
-				return;
+			//check if speed accedes max speed and slow down other wheel instead if it is 
+			if(p0 + kp > 1 || p0 + kp < -1) { 
+				correctionMod = -1;
+				followLeft = !followLeft;
 			}
 			
-			//drive at the new corrected speed
-			RobotMap.getChassis().tankDrive(p0 + lCorrection, p0 + rCorrection);
+			//drive at new corrected speed
+			if(followLeft)
+				RobotMap.getChassis().tankDrive(p0, p0 + kp * correctionMod);
+			else
+				RobotMap.getChassis().tankDrive(p0 + kp * correctionMod, p0);
+
 			
 		}
 		//stop
