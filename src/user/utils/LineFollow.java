c608a46ad8ac93043c2907687b9c1c21380c2057
simplyCyclops <maipez04@gmail.com>
2019-11-14 22:56:34 +0200

    added follower seconds and descriptions

--- src/user/utils/LineFollow.java
+++ src/user/utils/LineFollow.java
@@ -1,10 +1,24 @@
 package user.utils;
 
+
 import robot.RobotMap;
+
+import robot.runs.parallel.Action;
+import robot.runs.parallel.Parallel;
 
 public class LineFollow {
 	
-	public static void followDegrees(int p0, int distance, int light, double kp, boolean followLeft, boolean brake) {
+	/**
+	 * Follows a line for a certain distance
+	 * @param p0	the driving speed
+	 * @param distance	the distance to drive for in CM
+	 * @param light 	the light level for the sensor to follow
+	 * @param kp	the correction intensity, a high value will result in sharper turns 
+	 * @param followLeft	setting this to false will follow the right light sensor 
+	 * @param brake 	true - brake at the end, false - coast at the end
+	 */
+	public static void followDegrees(int p0, int distance, int light, double kp, boolean followLeft,
+			boolean brake) {
 		
 		//reset the wheels' rotation sensor
 		RobotMap.getMotor("lWheel").resetEncoder();
@@ -54,6 +68,62 @@ public class LineFollow {
 		//stop
 		General.stopRobot(brake);
 
+	}
+	
+	/**
+	 * Follows a line for a certain distance
+	 * @param p0	the driving speed
+	 * @param time	how long to drive for in seconds
+	 * @param light 	the light level for the sensor to follow
+	 * @param kp	the correction intensity, a high value will result in sharper turns 
+	 * @param followLeft	setting this to false will follow the right light sensor 
+	 * @param brake 	true - brake at the end, false - coast at the end
+	 */
+	public static void followSeconds(int p0, double time, int light, double kp, boolean followLeft,
+			boolean brake) {
+			
+		String sensor = "lColor";
+		if(!followLeft) sensor = "rColor"; //determines the sensor to be followed
+		
+		int correctionMod; //the modifier for adding / subtracting speed
+		
+		//start drive
+		RobotMap.getChassis().tankDrive(p0, p0);
+		
+		//calculate when to stop driving
+		long targetTime = (long) (System.currentTimeMillis() + (time * 1000)); 
+		
+		//drive until time is up
+		while (targetTime > System.currentTimeMillis()) {
+			
+			correctionMod = 1; //sets the correction modifier to add speed
+			
+			//if sensor is on color - move in opposite direction
+			if(RobotMap.getSensor(sensor).read() < light + 0.05
+					|| RobotMap.getSensor(sensor).read() > light - 0.05) {
+				followLeft = true;
+			}
+			//if sensor is not on color move toward color
+			else {
+				followLeft = false;
+			}
+			
+			//check if speed accedes max speed and slow down other wheel instead if it is 
+			if(p0 + kp > 1 || p0 + kp < -1) { 
+				correctionMod = -1;
+				followLeft = !followLeft;
+			}
+			
+			//drive at new corrected speed
+			if(followLeft)
+				RobotMap.getChassis().tankDrive(p0, p0 + kp * correctionMod);
+			else
+				RobotMap.getChassis().tankDrive(p0 + kp * correctionMod, p0);
+		}
+			
+		//stop
+		General.stopRobot(brake);
+	
 	}
 	
 }
