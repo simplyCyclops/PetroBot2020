3c1cd225847dba1a1f3504b9a50d8d1cc93fe106
simplyCyclops <maipez04@gmail.com>
2019-11-15 10:24:00 +0200

    testing

--- src/user/runs/TestRun.java
+++ src/user/runs/TestRun.java
@@ -1,6 +1,9 @@
 package user.runs;
 
+import robot.RobotMap;
 import robot.runs.RobotRun;
+import user.utils.Acceleration;
+import user.utils.Acceleration.Accelerator;
 
 public class TestRun extends RobotRun {
 
@@ -12,6 +15,11 @@ public class TestRun extends RobotRun {
 	public void runInstructions() {
 		// This run is for testing ideas on the fly | do not delete
 		
+		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 600, true);
+		Acceleration.accelerateSeconds(new Accelerator(0, -0.3), 0.6, true);
+		//RobotMap.getChassis().backwardDrive(0.1, 150, true);
+		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 250, true);
+		RobotMap.getMotor("lArm").rotateDegrees(.8, .8, 1000, true);
 	}
 
 }
