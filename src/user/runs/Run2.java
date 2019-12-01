d6d4a8cc6cd0e3fa47e3127cff00fde4e8f6f1d3
LPNG1 <itamar.oryan@gmail.com>
2019-12-01 17:14:54 +0200

    progress

--- src/user/runs/Run2.java
+++ src/user/runs/Run2.java
@@ -18,16 +18,22 @@ public class Run2 extends RobotRun {
 		
 		RobotMap.getSensor("gyro").resetToCurrentValue();
 		
-		RobotMap.getChassis().tankDriveDegrees(0.8, 0.8, 0.2, Conversion.cmToDegrees(51), true);
+		RobotMap.getChassis().tankDriveDegrees(0.8, 0.8, 0.2, Conversion.cmToDegrees(50), true);
 		GyroTurn.turnInPlace(0.1, 47, true);
-		NewGyroFollow.followDegrees(0.8, 1.0, 0.012, Conversion.cmToDegrees(30), 47, false);
-		NewGyroFollow.followDegrees(0.8, 0.2, 0.012, Conversion.cmToDegrees(98), 47, false);
+		NewGyroFollow.followDegrees(0.8, 1.0, 0.012, Conversion.cmToDegrees(128), 47, false);
+		RobotMap.getMotor("lWheel").setAcceleration(0.2);
+		RobotMap.getMotor("rWheel").setAcceleration(0.2);
+		RobotMap.getChassis().brake();
 		
 		Wait.waitForSeconds(0.5);
 		
-		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 650, true);
-		RobotMap.getChassis().backwardDriveDegrees(0.2, 0.4, 150, true);
-		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 300, true);
+		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 630, true);
+		RobotMap.getChassis().backwardDriveDegrees(0.2, 0.4, 130, true);
+		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 530, true);
+		
+		RobotMap.getChassis().backwardDriveDegrees(0.3, 0.4, 350, true);
+		GyroTurn.turnInPlace(0.1, 67, true);
+		RobotMap.getChassis().forwardDriveDegrees(0.6, 0.4, 520, true);
 	}
 
 }
