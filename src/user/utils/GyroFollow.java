10691fb3618f5269292786114b0cc9a575820944
simplyCyclops <maipez04@gmail.com>
2020-01-26 20:56:37 +0200

    follow seconds - doesn't work

--- src/user/utils/GyroFollow.java
+++ src/user/utils/GyroFollow.java
@@ -1,6 +1,7 @@
 package user.utils;
 
 import robot.RobotMap;
+import robot.runs.RunHandler;
 import robot.utils.Condition;
 
 public class GyroFollow {
@@ -27,5 +28,25 @@ public class GyroFollow {
 			if(brake) RobotMap.getChassis().brake();
 			else RobotMap.getChassis().coast();
 			
+		}
+		
+		public static void followSeconds(double p0, double acceleration, double kp, double ki, double seconds, int target, boolean brake) {
+			int error = 0;
+			int integral = 0;
+			
+			long start = System.currentTimeMillis();
+			
+			while(RunHandler.isRunning() && System.currentTimeMillis() - start == (long)(seconds * 100)) {	
+				System.out.println(System.currentTimeMillis() - start);
+				System.out.println((long)(seconds * 100));
+				System.out.println(System.currentTimeMillis() - start == (long)(seconds * 100));
+				error = (int) (RobotMap.getSensor("gyro").read() - target);
+				integral += error;
+				double correction = General.clampSpeed((error * kp) + (integral * ki));
+				if (correction > 0) RobotMap.getChassis().tankDrive(p0 - correction, p0, acceleration);
+				else RobotMap.getChassis().tankDrive(p0, p0 + correction, acceleration);
+			}
+			
+			General.stopRobot(brake);
 		}
 }
