4216b2fa8f824702f74cdbf35cba05be270e704f
simplyCyclops <maipez04@gmail.com>
2020-01-28 14:10:17 +0200

    added color reset

---
+++ src/user/runs/ColorReset.java
@@ -0,0 +1,36 @@
+package user.runs;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import robot.RobotMap;
+import robot.runs.RobotRun;
+import robot.utils.Wait;
+import user.utils.LineFollow;
+
+public class ColorReset extends RobotRun{
+
+	public ColorReset(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void runInstructions() {
+		LCD.drawString("L on black", 0, 0);
+		
+		Button.waitForAnyPress();
+		
+		LineFollow.black = RobotMap.getSensor("lColor").read();
+		LCD.clear();
+		Wait.waitForSeconds(0.2);
+		LCD.drawString("L on white", 0, 0);
+		
+		Button.waitForAnyPress();
+		
+		LineFollow.white = RobotMap.getSensor("lColor").read();
+		LCD.clear();
+		Wait.waitForSeconds(0.2);
+
+	}
+
+}
