eeaf269cb11093721243f2d4a3d4a227013812a5
simplyCyclops <maipez04@gmail.com>
2020-02-09 16:35:38 +0200

    moved run 2 from testrun to Run2

--- src/user/runs/Run2.java
+++ src/user/runs/Run2.java
@@ -1,11 +1,9 @@
 package user.runs;
 
-import lejos.hardware.Button;
 import robot.RobotMap;
 import robot.runs.RobotRun;
 import robot.utils.Action;
 import robot.utils.Wait;
-import user.utils.CircleTurn;
 import user.utils.General.Conversion;
 import user.utils.GyroFollow;
 import user.utils.GyroTurn;
@@ -20,9 +18,19 @@ public class Run2 extends RobotRun {
 	@Override
 	public void runInstructions() {
 		
-		RobotMap.getSensor("gyro").resetToCurrentValue();
+		//reset gyro
+		RobotMap.getSensor("gyro").resetToCurrentValue();;
+
+		//drive away from wall
+		RobotMap.getChassis().tankDriveDegrees(0.4, 0.4, 0.4, Conversion.cmToDegrees(19), true);
 		
-		RobotMap.getChassis().forwardDriveDegrees(0.2, 0.6, Conversion.cmToDegrees(9), false);
+		//turn 90 degrees
+		GyroTurn.turnTo(0.1, 89);
+		
+		//drive forward towards line
+		GyroFollow.followDegrees(0.3, 0.4, 0.03, 0, Conversion.cmToDegrees(27), 89, false);
+
+		//open arm
 		new Action() {
 			
 			@Override
@@ -31,38 +39,48 @@ public class Run2 extends RobotRun {
 			}
 		}.runInParallel();
 		
-		Wait.waitForSeconds(0.3);
-		CircleTurn.turn(0.4, 0.6, 26, 97, "right", false);
+		//follow line at the same time
+		LineFollow.followDegrees(0.4, Conversion.cmToDegrees(90), 0.04, "rColor", "right", true);
+		
+		//straighten
+		GyroTurn.turnTo(0.1, 93);
+		
+		//collide with swing
+		RobotMap.getChassis().tankDriveDegrees(0.5, 0.5, 0.4, Conversion.cmToDegrees(28), true);
 				
-		LineFollow.followDegrees(0.3, Conversion.cmToDegrees(20), 0.15, "rColor", "right");
-		GyroTurn.turnTo(0.1, 90);
-		System.out.println(RobotMap.getSensor("gyro").read());
+		//drive backward
+		RobotMap.getChassis().tankDriveDegrees(-0.4, -0.4, Conversion.cmToDegrees(10), true);
 		
-		GyroFollow.followDegrees(0.4, 0.3, 0.02, 0, Conversion.cmToDegrees(85), 90, true);	//false?
-		GyroTurn.turnTo(0.1, 90);
-
-		Button.waitForAnyPress();
+		//turn 90 degrees
+		GyroTurn.turnTo(0.1, -1);
 		
-		GyroFollow.followDegrees(0.2, 0.3, 0.02, 0, Conversion.cmToDegrees(32.2), 90, true);	//false?
-
-		//RobotMap.getChassis().tankDriveDegrees(0.2, 0.2, 0.5, Conversion.cmToDegrees(27.2), true);
-		Wait.waitForSeconds(0.5);
+		//drive back to straighten on wall
+		RobotMap.getChassis().tankDriveDegrees(-0.3, -0.3, Conversion.cmToDegrees(30), true);
+		Wait.waitForSeconds(0.3);
 		
+		//reset gyro
+		RobotMap.getSensor("gyro").resetToCurrentValue();
 		
-		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 650, true);
+		//drive forward
+		RobotMap.getChassis().tankDriveDegrees(0.3, 0.3, Conversion.cmToDegrees(8), false);
+		GyroFollow.followDegrees(0.2, 0.4, 0.02, 0, Conversion.cmToDegrees(34), -1, true);
+				
+		//turn towards safety factor
+		GyroTurn.turnTo(0.1, 87);
+		
+		//drive towards safety factor
+		GyroFollow.followDegrees(0.3, 0.3, 0.02, 0, Conversion.cmToDegrees(25), 89, true);
+		Wait.waitForSeconds(0.3);
+		
+		//do safety factor
+		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 620, true);
 		RobotMap.getChassis().backwardDriveDegrees(0.2, 0.4, Conversion.cmToDegrees(3.5), true);
 		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 450, true);
 		
-		RobotMap.getChassis().tankDriveDegrees(0.4, -0.4, 0.1, 107, true);
-		RobotMap.getChassis().forwardDriveDegrees(0.4, 0.6, Conversion.cmToDegrees(15), true);
-		RobotMap.getMotor("lArm").rotateDegrees(-0.8, 500, true);
 		Wait.waitForSeconds(0.3);
-		RobotMap.getMotor("lArm").rotateDegrees(0.8, 500, true);
 		
-		RobotMap.getChassis().tankDriveDegrees(-0.4, 0.4, 0.6, 98, false);
-
-		RobotMap.getChassis().tankDriveDegrees(-0.8, -0.8, 0.6, Conversion.cmToDegrees(155), false);
-		RobotMap.getChassis().tankDriveDegrees(-0.8, -0.85, 0.6, Conversion.cmToDegrees(100), false);
+		//drive back to home
+		GyroFollow.followDegrees(-0.6, 0.6, 0.02, 0, Conversion.cmToDegrees(230), 88, false);
 
 	}
 
--- src/user/runs/TestRun.java
+++ src/user/runs/TestRun.java
@@ -19,69 +19,6 @@ public class TestRun extends RobotRun {
 	public void runInstructions() {
 		// This run is for testing ideas on the fly | do not delete
 		
-		//reset gyro
-		RobotMap.getSensor("gyro").resetToCurrentValue();;
-
-		//drive away from wall
-		RobotMap.getChassis().tankDriveDegrees(0.4, 0.4, 0.4, Conversion.cmToDegrees(19), true);
-		
-		//turn 90 degrees
-		GyroTurn.turnTo(0.1, 89);
-		
-		//drive forward towards line
-		GyroFollow.followDegrees(0.3, 0.4, 0.03, 0, Conversion.cmToDegrees(27), 89, false);
-
-		//open arm
-		new Action() {
-			
-			@Override
-			public void execute() {
-				RobotMap.getMotor("lArm").rotateDegrees(-0.8, 1000, true);
-			}
-		}.runInParallel();
-		
-		//follow line at the same time
-		LineFollow.followDegrees(0.4, Conversion.cmToDegrees(90), 0.04, "rColor", "right", true);
-		
-		//straighten
-		GyroTurn.turnTo(0.1, 93);
-		
-		//collide with swing
-		RobotMap.getChassis().tankDriveDegrees(0.5, 0.5, 0.4, Conversion.cmToDegrees(28), true);
-				
-		//drive backward
-		RobotMap.getChassis().tankDriveDegrees(-0.4, -0.4, Conversion.cmToDegrees(10), true);
-		
-		//turn 90 degrees
-		GyroTurn.turnTo(0.1, -1);
-		
-		//drive back to straighten on wall
-		RobotMap.getChassis().tankDriveDegrees(-0.3, -0.3, Conversion.cmToDegrees(30), true);
-		Wait.waitForSeconds(0.3);
-		
-		//reset gyro
-		RobotMap.getSensor("gyro").resetToCurrentValue();
-		
-		//drive forward
-		RobotMap.getChassis().tankDriveDegrees(0.3, 0.3, Conversion.cmToDegrees(8), false);
-		GyroFollow.followDegrees(0.2, 0.4, 0.02, 0, Conversion.cmToDegrees(35), -1, true);
-				
-		//turn towards safety factor
-		GyroTurn.turnTo(0.1, 87);
-		
-		//drive towards safety factor
-		GyroFollow.followDegrees(0.3, 0.3, 0.02, 0, Conversion.cmToDegrees(29), 89, true);
-		Wait.waitForSeconds(0.3);
-		
-		//do safety factor
-		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 650, true);
-		RobotMap.getChassis().backwardDriveDegrees(0.2, 0.4, Conversion.cmToDegrees(3.5), true);
-		RobotMap.getMotor("lArm").rotateDegrees(0.6, 0.5, 450, true);
-		
-		Wait.waitForSeconds(0.3);
-		
-		//drive back to home
-		GyroFollow.followDegrees(-0.6, 0.6, 0.02, 0, Conversion.cmToDegrees(230), 88, false);
 	}
 
 }
