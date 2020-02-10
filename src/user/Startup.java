ecf81b247eff7758b9f03f4817464fe68f15431e
simplyCyclops <maipez04@gmail.com>
2020-02-10 09:13:33 +0200

    updated startup

--- src/user/Startup.java
+++ src/user/Startup.java
@@ -5,11 +5,11 @@ import robot.hardware.motors.LargeMotor;
 import robot.hardware.sensors.ColorSensor;
 import robot.hardware.sensors.GyroSensor;
 import user.runs.ColorReset;
-import user.runs.GyroCheckRun;
+import user.runs.GyroReset;
 import user.runs.Run1;
-import user.runs.Run4;
 import user.runs.Run2;
 import user.runs.Run3;
+import user.runs.Run4;
 import user.runs.TestRun;
 import user.utils.GyroFollow;
 
@@ -28,13 +28,13 @@ public class Startup {
 	}
 
 	public static void createRuns() {
-		new ColorReset("Color Reset");
+		
+		new GyroReset("- Gyro Reset -");
+		new ColorReset("- Color Reset -");
 		new Run1("Run 1");
 		new Run2("Run 2");
 		new Run3("Run 3");
 		new Run4("Run 4");
-
-		new GyroCheckRun("[Gyro Test]");
 		
 		// this run is for testing idea on the fly | do not delete | keep at the bottom
 		new TestRun("[Test Run]");
